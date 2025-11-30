bb67605c97028f518c9a88dd079264d07811f576
solnutzlol-te <solnutzlol@gmail.com>
2025-11-30 10:19:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/home";
 import MemePage from "./pages/meme-page";
-import ProfilePage from "./pages/profile";
 import NotFound from "./pages/404";
 
 const queryClient = new QueryClient();
@@ -17,8 +16,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/meme-generator" element={<MemePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
