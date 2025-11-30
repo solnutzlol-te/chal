215aa35d4e4c253d7dad23917c73bca3b00a4b52
solnutzlol-te <solnutzlol@gmail.com>
2025-11-30 04:26:55 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -20,7 +20,7 @@ import Navbar from "@/components/navbar";
 
 export default function Index() {
   const totalSupply = 1999;
-  const mintedCount = 138; 
+  const mintedCount = 300; 
   const price = 0; 
 
   const nftGallery = [
